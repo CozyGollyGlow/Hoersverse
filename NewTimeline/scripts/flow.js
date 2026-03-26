7475b059a06e088b57d5bd61d735fff287c7057a
CozyGollyGlow <cozygollyglow@protonmail.com>
2026-03-26 23:53:54 +0100

    put JS in script files

---
+++ NewTimeline/scripts/flow.js
@@ -0,0 +1,85 @@
+const flows = [
+    {
+        name: "World Events",
+        color: "#808080",
+        events: [
+            { start: 2010, label: "Return of Princess Luna" },
+            { start: 2036, label: "Twilight elected Prime Minister" },
+            { start: 2045, label: "Launch of Stardust 21" }
+        ]
+    },
+    {
+        name: "Spitfire",
+        color: "#ffa500",
+        lifetime: { start: 1982, label: "Born 13th November, 1982" },
+        events: [
+            { start: 2008, end: 2014, label: "Captain of the Wonderbolt" }
+        ]
+    },
+    {
+        name: "Concorde",
+        color: "#d1f0ff",
+        lifetime: { start: 2017, label: "December 17th, 2017" },
+        events: [
+            { start: 2029, label: "Gets their cutie mark" },
+            { start: 2038, label: "Kwadube Observatory Visit" },
+            { start: 2035, end: 2038.88, label: "University" },
+            { start: 2039, end: 2044, label: "EASA Astronaut Group 42 Training" },
+            { start: 2044.12, end: 2064, label: "EASA Astronaut" }
+        ]
+    },
+    {
+        name: "Aurora",
+        color: "#ffe49d",
+        lifetime: { start: 2015, label: "Birth date unknown (~2015 ?)" },
+        events: [
+            { start: 2044.12, end: 2064, label: "EASA Astronaut" }
+        ]
+    },
+    {
+        name: "Rosetta",
+        color: "#ce99e9",
+        lifetime: { start: 2003, label: "Birth date unknown (~2003 ?)" },
+        events: [
+            { start: 2038, label: "Kwadube Observatory Visit" },
+            { start: 2044.12, end: 2064, label: "EASA Astronaut" }
+        ]
+    },
+    {
+        name: "Zambuko",
+        color: "#d8d8d8",
+        lifetime: { start: 2014, label: "Birth date unknown (~2014 ?)" },
+        events: [
+            { start: 2044.12, end: 2064, label: "EASA Astronaut" }
+        ]
+    },
+    {
+        name: "Wolfram",
+        color: "#9fbacd",
+        lifetime: { start: 2005, label: "Birth date unknown (~2005 ?)" },
+        events: [
+            { start: 2044.12, end: 2064, label: "EASA Astronaut" }
+        ]
+    },
+    {
+        name: "Atom Smasher",
+        color: "#35c965",
+        lifetime: { start: 2052, label: "Birth date estimated to 2052" },
+        events: [
+        ]
+    },
+    {
+        name: "Rainbow Code",
+        color: "#e4c993",
+        lifetime: { start: 2041, label: "Birth date estimated to 2041" },
+        events: [
+        ]
+    },
+    {
+        name: "Candy Chip",
+        color: "#ffb9d6",
+        lifetime: { start: 2052, label: "Birth date unknown" },
+        events: [
+        ]
+    }
+];
---
+++ NewTimeline/scripts/script.js
@@ -0,0 +1,87 @@
+const tlStart = 1980; // Start of the timeline (will be the offset of everything)
+const container = document.getElementById("timeline");
+const scale = 25; // pixel per time unit
+container.style.setProperty("--scale", scale + "px");
+
+const maxTime = Math.max(
+    ...flows.flatMap(f =>
+        f.events.map(e => e.end ?? e.start)
+    )
+);
+
+container.style.width = ((maxTime - tlStart + 1) * scale) + "px";
+
+// Generate decades
+const axis = document.getElementById("axis");
+const decadeStep = 10; // 10 time units = decade
+
+for (let t = 0; t <= maxTime - tlStart; t += decadeStep) {
+    const tick = document.createElement("div");
+    tick.className = "tick";
+    tick.style.left = (t * scale) + "px";
+    tick.textContent = t + tlStart; // or "1900", etc.
+
+    axis.appendChild(tick);
+}
+
+flows.forEach(flow => {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    const rowName = document.createElement("p");
+    rowName.className = "row-name";
+    rowName.innerHTML = `${flow.name}`
+    row.appendChild(rowName)
+
+    if(flow.lifetime) {
+	    const rowLifetime = document.createElement("div");
+	    rowLifetime.className = "row-lifetime";
+
+	    const lifetimeX = (flow.lifetime.start - tlStart) * scale;
+	    rowLifetime.style.left = lifetimeX + "px";
+	    if(flow.lifetime.end){
+	    	rowLifetime.style.width = ((flow.lifetime.end - flow.lifetime.start) * scale) + "px";
+	    } else {
+	    	rowLifetime.style.width = ((maxTime - flow.lifetime.start) * scale) + "px";
+	    }
+
+	    rowLifetime.innerHTML = flow.lifetime.label;
+        rowLifetime.style.background = flow.color;
+	    row.appendChild(rowLifetime)
+	}
+
+    const rowEvents = document.createElement("div");
+    rowEvents.className = "row-events";
+    row.appendChild(rowEvents)
+
+    flow.events.forEach(e => {
+        const x = (e.start - tlStart) * scale;
+
+        if (e.end !== undefined && e.end !== e.start) {
+            // Event during in the time
+            const eventDiv = document.createElement("div");
+            eventDiv.className = "event";
+
+            eventDiv.style.left = x + "px";
+            eventDiv.style.width = ((e.end - e.start) * scale) + "px";
+            eventDiv.style.background = flow.color;
+            eventDiv.textContent = e.label;
+
+            rowEvents.appendChild(eventDiv);
+        } else {
+        	// Punctual event
+            const dot = document.createElement("div");
+			dot.className = "event-dot";
+			dot.style.left = x + "px";
+
+			const label = document.createElement("span");
+			label.className = "event-dot-label";
+			label.textContent = e.label;
+
+			dot.appendChild(label);
+			rowEvents.appendChild(dot);
+        }
+    });
+
+    container.appendChild(row);
+});
